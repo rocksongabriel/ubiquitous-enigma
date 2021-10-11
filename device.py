b2d38bab697678771b512f100524307ccd81b082
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-11 23:40:42 +0000

    Add current_assigned_job attribute and methods

--- device.py
+++ device.py
@@ -13,6 +13,7 @@ class Device:
         self.number_of_tasks = 0
         self.pheromone_level = 0
         self.caculate_pheromone_level() # calculate the pheromone level when the device is created
+        self.current_assigned_job = 0 # zero means no job jas been assigned currently, this will be set by the server
 
     def increase_number_of_tasks(self):
         self.number_of_tasks += 1
@@ -28,6 +29,16 @@ class Device:
     @property
     def get_pheromone_level(self):
         return self.pheromone_level
+
+    def set_current_assigned_job(self, job_id):
+        self.current_assigned_job = job_id
+
+    @property
+    def get_current_assigned_job(self):
+        return self.current_assigned_job
+
+    def remove_current_assigned_job(self):
+        self.current_assigned_job = 0
 
     def show_device_details(self):
         return f"Device ID: {self.device_id}, Processing Power: {self.processing_power}, Number of Tasks: {self.number_of_tasks}, Pheromone Level: {self.pheromone_level}"
