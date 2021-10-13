638fbffcc962376d23ae731258f325cd5df78c36
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-13 07:06:29 +0000

    Display the menu in a tabular form

--- ant.py
+++ ant.py
@@ -4,6 +4,7 @@ import time
 
 from pynput.keyboard import Controller, Key, Listener
 from rich.console import Console
+from rich.table import Table
 
 from device import Device
 from server import Server
@@ -35,12 +36,22 @@ def server_start_info():
     console.print("\n[bold yellow]Server running ...[bold yellow]")
 
 def program_menu():
-    console.print("\nPress 'a'\t\t: [bold blue]to add a device to this server.[bold blue]")
-    console.print("Press 'b'\t\t: [bold blue]to manually assign a job.[bold blue]")
-    console.print("Press 'm'\t\t: [bold blue]to show this menu.[bold blue]")
-    console.print("Press 's'\t\t: [bold blue]to show all devices on this server.[bold blue]")
-    console.print("Press 'space bar'\t: [bold blue]to automatically assign a job.[bold blue]")
-    console.print("[bold red]Press 'esc' (escape key) to quit the server.[bold red]")
+    table = Table(title="[bold yellow]Menu of Ant Colony Optimization Terminal Program.[bold yellow]")
+
+    table.add_column("Command", justify="center", style="cyan", no_wrap=True)
+    table.add_column("Purpose", style="blue", no_wrap=True)
+
+    table.add_row("Press 'a'", "[bold blue]add a device to this server.[bold blue]")
+    table.add_row("Press 'b'", "[bold blue]to manually assign a job.[bold blue]")
+    table.add_row("Press 'd", "[bold blue]show all devices on this server.[bold blue]")
+    table.add_row("Press 'm'", "[bold blue]show this menu.[bold blue]")
+    table.add_row("Press 's'", "[bold blue]show the status of the server.[bold blue]")
+    table.add_row("Press 'space bar'", "[bold blue]automatically assign a job.[bold blue]")
+    table.add_row("Press 'esc' (escape key)", "[bold red]quit the server and stop the program.[bold red]")
+
+    console.print(table)
+
+    console.print("[green]Waiting for your input ... [/green]")
 
 
 def manual_job_creation(server):
@@ -73,13 +84,16 @@ def app():
     # Listen to Keyboard
     def on_press(key):
         try:
-            if key.char == 's':
+            if key.char == 'd':
                 clear_input_key()
                 server.show_devices() # display all devices
             elif key.char == 'a':
                 add_device_to_server(server) # add new device to the server
             elif key.char == 'b':
                 manual_job_creation(server) # manually create a job, and let the server assign a device
+            elif key.char == 's': 
+                clear_input_key()
+                server.show_current_status() # display the current status of the server
             elif key.char == 'm':
                 clear_input_key()
                 program_menu()
